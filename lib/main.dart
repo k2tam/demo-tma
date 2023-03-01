e1c4827096bd20d58393e472e0fa5608043b6acf
k2tam <buingockhaitam01@gmail.com>
2023-03-01 11:54:43 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  const Text('Connect to Device'),
+                  const Text(
+                    'Connection Failed',
+                    style: TextStyle(fontWeight: FontWeight.bold),
+                  ),
+                  const Text('Check your local network connectivity'),
+                  const Text(
+                    "Power cycle your Ambi Climate device and wait for 30 seconds, then press 'Try Again' ",
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 3,
+              child: Stack(children: [
+
+                Center(
+                  child: Container(
+                    width: 340,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 255, 239, 238),
+                      shape: BoxShape.circle
+                    ),
+                  ),
+                ),
+
+                Center(
+                  child: Container(
+                    width: 320,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 240, 228, 227),
+                      shape: BoxShape.circle
+                    ),
+                  ),
+                ),
+
+                Center(
+                  child: Container(
+                    width: 300,
+                    decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 227, 195, 193),
+                      shape: BoxShape.circle
+                    ),
+                  ),
+                ),
+                
+                Center(
+                  child: Icon(
+                    Icons.wifi_find_outlined,
+                    size: 180
+                  ),
+                ),
+              ]),
+            ),
+            Container(
+                width: 300,
+                height: 50,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Color.fromARGB(255, 16, 144, 82),
+                ),
+                child:
+                    // ElevatedButton(onPressed: () {}, child: Text('Try Again'))),
+                    TextButton(
+                  child: Text(
+                    'Try again',
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                  ),
+                  onPressed: () {},
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
